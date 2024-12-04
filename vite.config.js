b52b24c8bf1ae50b87175f5d2459e480d6231a45
nigelp <nigelp@users.noreply.github.com>
2024-12-04 12:50:15 +0000

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: 3000,
-    open: false,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
